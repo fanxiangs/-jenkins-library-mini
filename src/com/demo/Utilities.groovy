936a5bd2bb736611055b697f2ae5ace35ad5e8ad
fanxiangs <fanxiang1378@gmail.com>
2023-06-10 17:22:43 +0000

    add returnStatus

--- src/com/demo/Utilities.groovy
+++ src/com/demo/Utilities.groovy
@@ -3,9 +3,9 @@ package com.demo
 class Utilities {
     static def exec(script, args) {       
         script.timestamps {
-            script.sh "ls  -al"
-            script.echo "${script.env.HOME}  ${args}"
-            script.help
+            def result = script.sh(returnStatus: true, script: 'ls -al')
+            script.echo "${script.env.HOME}  ${args} result: ${result}"
+            script.pwd
         }
     }
 }
