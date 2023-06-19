ac00381abae2aaa34fd6017f107fb87abdc65d01
Xiang Fan <45086129+fanxiangs@users.noreply.github.com>
2023-06-19 16:04:34 +0800

    add VersionUtils

---
+++ vars/versionUtils.groovy
@@ -0,0 +1,33 @@
+import org.yaml.snakeyaml.DumperOptions
+import org.yaml.snakeyaml.Yaml
+
+class VersionUtils {
+    String versionYamlFilePath = 'VERSION'
+    
+    def readVersionYaml() {
+        def versionYamlContent = readFile(versionYamlFilePath)
+        def versionMap = new Yaml().load(versionYamlContent)
+        return versionMap
+    }
+    
+    def updatePreRelease() {
+        def versionMap = readVersionYaml()
+        
+        def preRelease = versionMap.VERSION_PRERELEASE ?: 0
+        
+        // Increment the preRelease value by 1
+        preRelease++
+        
+        // Update the preRelease value in the versionMap
+        versionMap.VERSION_PRERELEASE = preRelease
+        
+        // Write the updated versionMap back to the VERSION file
+        def dumperOptions = new DumperOptions()
+        dumperOptions.setDefaultFlowStyle(DumperOptions.FlowStyle.BLOCK)
+        def yaml = new Yaml(dumperOptions)
+        def updatedVersionYamlContent = yaml.dump(versionMap)
+        writeFile file: versionYamlFilePath, text: updatedVersionYamlContent
+        
+        return preRelease
+    }
+}
